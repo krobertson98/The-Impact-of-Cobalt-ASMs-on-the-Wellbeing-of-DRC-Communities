6305d808776d7431ccf8077b9de9a3779d1d1732
krobertson98 <kalebrobertson20@gmail.com>
2022-04-27 00:13:04 -0400

    cleaning up files fixed part of population

--- DRC APA/population.py
+++ DRC APA/population.py
@@ -19,44 +19,53 @@ print("\nList Of Provinces:\n", pop.index)
 #getting the population total pop for each province
 print("Population in each Province",pop['Pop_2020'])
 print("Total Population of DRC (2020)",pop['Pop_2020'].sum())
-#%%
-#getting the amount of internally displaced people in countries as an indicator for 
-con = pd.read_excel('rdc_mouvement_de_population_deplace_juin_2021.xlsx')
-con = con.rename( columns={'creation_date':'date','admin1Name_fr':'province','person':'Pop_2020'} )
+
+#%%getting the amount of people returning back to provinces
+con = pd.read_excel('rdc_mouvement_de_population_deplace_octobre_2021.xlsx')
+con = con.rename( columns={'creation_date':'date','admin1_label':'province'} )
+
 ymd = pd.to_datetime(con["date"],format="%m%d%y")
 #only including the year 2020. 2020 is the most recent year with a full set of data,matches year of previous files
 con["date"] = ymd.dt.to_period("Y")
-year_ok = ymd.dt.year >= 2020
-con["dateuse"] = con[year_ok==True]
-fight = con.groupby('province')
-con_prov = fight['Pop_2020']
-print("Internally displaced people by province:",con_prov)
-print("Total displaced people in DRC (2020):",con_prov.sum())
-#checking if there is the same number and same number of province
-print("Number of provinces:",len(fight),"\nList of provinces:\n",fight.index)
-con_prov.to_csv("conflict.csv")
-
+year_ok = ymd.dt.year <= 2021
+dates = con[year_ok==True]
+fight = dates
+#dropping columns with other dates not needed due to using the date based off creation of the report since it is already evaulated to be in dataset
+fight = fight.drop(columns='evaluation_date')
+fight = fight.drop(columns='movement_date')
+con_prov = fight['person'].groupby(fight['province'])
+print("Internally displaced people by province:",con_prov.sum())
+tight = con_prov.sum()
+#this figure does not include displaced people before 2019 that was about 2.9 million people
+print("Total displaced people in DRC from 2019 to 2020:",fight['person'].sum())
+#getting new csv file without 2021 dates to use for gis layer
+tight.to_csv('conflict.csv')
 use = pd.read_csv("conflict.csv")
-join_keys = ["province","Pop_2020",] 
-#merging the datasets using a one to one outer join
-merged = use.merge(pop, on=join_keys , how="outer", validate="1:1", indicator=True)
-#getting the number of records in both data sets and the ones not in both.
-print(merged["_merge"].value_counts())
 
-merged.to_csv("join.csv",index=False)
+
+
 #%%
-join = pd.read_csv('join.csv')
-keep_prov = join["province"].isin(top_cand.index)
-keep_pov = join["Pop_2020"].isin(top_state.index)
-keep = keep_cand & keep_state
-sub = reset[keep]
-grouped = sub.groupby(["STATE","CAND_NAME"])
-summed = grouped['amt'].sum()
-grid = summed.unstack("STATE")
-fig, ax1 = plt.subplots(dpi=300)
-fig.suptitle("Contributions in Millions")
-sns.heatmap(grid, annot=True,fmt=".0f", ax=ax1)
-ax1.set_xlabel("State")
-ax1.set_ylabel("Row")
+
+in_group = fight['province'].isin(['Maniema','Haut-uele','Nord-kivu'])
+subset = fight[ in_group ].sort_values('person')
+
+fig, ax1 = plt.subplots()
+sns.barplot(data=subset,x='province',y='person',ax=ax1)
+plt.xticks(rotation = 45)
+ax1.set_title("Interanlly Displaced People in DRC by Province 2019-2020")
+ax1.set_xlabel("Provinces")
+ax1.set_ylabel("Internally Displaced People")
 fig.tight_layout()
-fig.savefig("heatmap.png")
+fig.savefig('conflict.png')
+
+out_group = fight['province'].isin(['Maniema','Haut-uele','Nord-kivu'])==False
+subset2 = fight[ out_group ].sort_values('person')
+
+fig, ax1 = plt.subplots()
+sns.barplot(data=subset2,x='province',y='person',ax=ax1)
+plt.xticks(rotation = 45)
+ax1.set_title("Interanlly Displaced People in DRC by Province 2019-2020")
+ax1.set_xlabel("Provinces")
+ax1.set_ylabel("Internally Displaced People")
+fig.tight_layout()
+fig.savefig('conflict.png')
